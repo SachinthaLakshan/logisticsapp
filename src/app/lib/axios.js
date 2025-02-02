15eb40dd282527712a7dde54fce23cd86543de28
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2025-02-02 19:48:36 +0530

    server chnage tho local

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://logisticsappbackend-962b1cb545ad.herokuapp.com/api', // Replace with your API base URL
+    //baseURL: 'https://logisticsappbackend-962b1cb545ad.herokuapp.com/api', // Replace with your API base URL
+    baseURL: 'http://localhost:9001/api',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true // if your backend requires cookies
 });
