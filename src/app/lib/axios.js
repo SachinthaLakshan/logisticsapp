55520efca715373f352a00fee3918a293f1ea100
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2025-02-04 22:15:52 +0530

    base url chaged into server

--- src/app/lib/axios.js
+++ src/app/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://logisticsappbackend-962b1cb545ad.herokuapp.com/api', // Replace with your API base URL
-    baseURL: 'http://localhost:9001/api',
+    baseURL: 'https://logisticsappbackend.vercel.app/api', // Replace with your API base URL
+    //aseURL: 'http://localhost:9001/api',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true // if your backend requires cookies
 });
